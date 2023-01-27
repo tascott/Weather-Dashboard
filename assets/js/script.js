96ac1e73815d5d7c296e69668cc4eb775ffc0368
TomScott <contact@tascott.co.uk>
2023-01-27 12:45:07 +0000

    Starter files and layout

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+
+// Api call
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// api key
+// 72c81f035f8297343fdf3596162bbc5c
