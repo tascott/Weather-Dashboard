40eb831a9890d117006cdcf820da70cb1f824bb0
TomScott <contact@tascott.co.uk>
2023-01-27 20:35:25 +0000

    Starter functions to grab the weather data for next 5 days and make it manageable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,78 @@
-console.log('Hello World!');
+let searchInput = $('#search-input');
+let searchButton = $('#search-button');
+let apiKey = '72c81f035f8297343fdf3596162bbc5c';
 
-// Api call
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+let dayData = {
+    day0: [],
+    day1: [],
+    day2: [],
+    day3: [],
+    day4: [],
+    day5: [],
+    day6: []
+}
 
-// api key
-// 72c81f035f8297343fdf3596162bbc5c
+let currentCity;
+
+
+searchButton.on('click', function (e) {
+    e.preventDefault();
+    let searchInputValue = searchInput.val();
+    currentCity = searchInputValue;
+    let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputValue + '&appid=' + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        let sorted = [];
+        response.list.forEach(function (timeblock) {
+            var date = moment(timeblock.dt_txt).format('DD MM YY');
+            var tempMax = timeblock.main.temp_max;
+            var tempMin = timeblock.main.temp_min;
+            var humidity = timeblock.main.humidity;
+            var wind = timeblock.wind.speed;
+            var icon = timeblock.weather[0].icon;
+            sorted.push({   //pushing the data into the array
+                date: date,
+                tempMax: tempMax,
+                tempMin: tempMin,
+                humidity: humidity,
+                wind: wind,
+                icon: icon
+            });
+        });
+       // get the unique dates from the array
+        let uniqueDates = [...new Set(sorted.map(item => item.date))];
+        // push all the data that matches each unique date into a new array
+        uniqueDates.forEach(function (date) {
+            let day = sorted.filter(function (item) {
+                return item.date === date;
+            });
+            // push each day array into a new array
+            if (dayData.day0.length === 0) {
+                dayData.day0.push(day);
+            } else if (dayData.day1.length === 0) {
+                dayData.day1.push(day);
+            } else if (dayData.day2.length === 0) {
+                dayData.day2.push(day);
+            } else if (dayData.day3.length === 0) {
+               dayData.day3.push(day);
+            } else if (dayData.day4.length === 0) {
+                dayData.day4.push(day);
+            } else if (dayData.day5.length === 0) {
+                dayData.day5.push(day);
+            } else if (dayData.day6.length === 0) {
+                dayData.day6.push(day);
+            }
+        });
+        console.log(dayData);
+    });
+
+    // $.ajax({
+    //     url: 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInputValue + '&appid=' + apiKey,
+    //     method: 'GET'
+    // }).then(function (response) {
+    //     console.log('weather today?', response);
+    // });
+});
+
