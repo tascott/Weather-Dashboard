f8ebdf06cb6274f23766d1c76761406b215d54bd
TomScott <contact@tascott.co.uk>
2023-01-27 22:24:12 +0000

    More filtering of data, start of appending to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ let dayData = {
 
 let currentCity;
 
-
 searchButton.on('click', function (e) {
     e.preventDefault();
     let searchInputValue = searchInput.val();
@@ -24,18 +23,17 @@ searchButton.on('click', function (e) {
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
+        console.log('initial response', response)
         let sorted = [];
         response.list.forEach(function (timeblock) {
             var date = moment(timeblock.dt_txt).format('DD MM YY');
             var tempMax = timeblock.main.temp_max;
-            var tempMin = timeblock.main.temp_min;
             var humidity = timeblock.main.humidity;
             var wind = timeblock.wind.speed;
             var icon = timeblock.weather[0].icon;
             sorted.push({   //pushing the data into the array
                 date: date,
                 tempMax: tempMax,
-                tempMin: tempMin,
                 humidity: humidity,
                 wind: wind,
                 icon: icon
@@ -43,7 +41,7 @@ searchButton.on('click', function (e) {
         });
        // get the unique dates from the array
         let uniqueDates = [...new Set(sorted.map(item => item.date))];
-        // push all the data that matches each unique date into a new array
+        // push all the data that matches each unique date into a new array to sort later
         uniqueDates.forEach(function (date) {
             let day = sorted.filter(function (item) {
                 return item.date === date;
@@ -65,14 +63,66 @@ searchButton.on('click', function (e) {
                 dayData.day6.push(day);
             }
         });
-        console.log(dayData);
-    });
 
-    // $.ajax({
-    //     url: 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInputValue + '&appid=' + apiKey,
-    //     method: 'GET'
-    // }).then(function (response) {
-    //     console.log('weather today?', response);
-    // });
+        // Some more filtering of this crazy data set
+        filterData();
+    });
 });
+
+let filterData = function () {
+    // get the data from local storage
+
+    for (const key in dayData) {
+        if (dayData.hasOwnProperty(key)) {
+            let day = dayData[key];
+
+            day.forEach(function (timeblock) {
+                let maxTemp = Math.max.apply(null,
+                    timeblock.map(function (o) {
+                        return o.tempMax;
+                    }));
+                let maxHumidity = Math.max.apply(null,
+                    timeblock.map(function (o) {
+                        return o.humidity;
+                    }));
+                let wind = Math.max.apply(null,
+                    timeblock.map(function (o) {
+                        return o.wind;
+                    }));
+                renderDay(timeblock[0].date, maxTemp, maxHumidity, wind);
+                // console.log(timeblock[0].date, maxTemp, maxHumidity, wind)
+            });
+
+        }
+    }
+
+    // renderHTML();
+};
+
+//TODO: if it's close to midnight, the date will be wrong as the data[0] will be the next day
+
+let renderDay = function (date, maxTemp, maxHumidity, wind) {
+    let html = `
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">${date}</h5>
+            <p class="card-text">Max Temp: ${maxTemp}</p>
+            <p class="card-text">Humidity: ${maxHumidity}</p>
+            <p class="card-text">Wind: ${wind}</p>
+            <img src="http://openweathermap.org/img/w/${icon}.png" alt="weather icon">
+        </div>
+    </div>
+    `;
+    // console.log(html)
+    $('#forecast').append(html);
+};
+
+// Get todays weather
+// $.ajax({
+//     url: 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInputValue + '&appid=' + apiKey,
+//     method: 'GET'
+// }).then(function (response) {
+//     console.log('weather today?', response);
+// });
+
 
